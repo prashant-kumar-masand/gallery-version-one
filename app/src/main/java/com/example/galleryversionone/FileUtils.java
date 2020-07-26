a5328479079026aa894e921c0f7effb3c2d20c33
prashant.kumar <prashant.kumar@algoworks.com>
2020-07-26 11:57:33 +0530

    added methods for writing and reading image files in SendFilesFragment

--- app/src/main/java/com/example/galleryversionone/FileUtils.java
+++ app/src/main/java/com/example/galleryversionone/FileUtils.java
@@ -1,27 +1,69 @@
 package com.example.galleryversionone;
 
+import android.content.ContentResolver;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.text.DecimalFormat;
 
 public class FileUtils {
+
+    public static InputStream getThumbnailInputStream(ContentResolver cr, Long imageId) {
+        Bitmap originalBitmap, scaleDownBitmap;
+
+        originalBitmap = MediaStore.Images.Thumbnails.getThumbnail(
+                cr, imageId,
+                MediaStore.Images.Thumbnails.MICRO_KIND,
+                null);
+        System.out.println("before compression "+originalBitmap.getAllocationByteCount());
+        scaleDownBitmap = scaleDown(originalBitmap, 100, false);
+        System.out.println("after scale down "+scaleDownBitmap.getAllocationByteCount());
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        scaleDownBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        InputStream is = new ByteArrayInputStream(baos.toByteArray());
+        System.out.println("after compression "+baos.toByteArray().length);
+        return is;
+    }
+
+    public static Bitmap scaleDown(Bitmap realImage, float maxImageSize,
+                                   boolean filter) {
+        float ratio;
+        int width, height;
+        Bitmap newBitmap;
+        ratio = Math.min(
+                (float) maxImageSize / realImage.getWidth(),
+                (float) maxImageSize / realImage.getHeight());
+        /* ratio should be less than 1 to get scale down image */
+        if (ratio >= 1.0) {
+            return realImage;
+        }
+        width = Math.round((float) ratio * realImage.getWidth());
+        height = Math.round((float) ratio * realImage.getHeight());
+        newBitmap = Bitmap.createScaledBitmap(realImage, width, height, filter);
+        return newBitmap;
+    }
 
     public static String formatFileSize(long size) {
         String hrSize = null;
 
         double b = size;
-        double k = size/1024.0;
-        double m = ((size/1024.0)/1024.0);
-        double g = (((size/1024.0)/1024.0)/1024.0);
-        double t = ((((size/1024.0)/1024.0)/1024.0)/1024.0);
+        double k = size / 1024.0;
+        double m = ((size / 1024.0) / 1024.0);
+        double g = (((size / 1024.0) / 1024.0) / 1024.0);
+        double t = ((((size / 1024.0) / 1024.0) / 1024.0) / 1024.0);
 
         DecimalFormat dec = new DecimalFormat("0.00");
 
-        if ( t>1 ) {
+        if (t > 1) {
             hrSize = dec.format(t).concat(" TB");
-        } else if ( g>1 ) {
+        } else if (g > 1) {
             hrSize = dec.format(g).concat(" GB");
-        } else if ( m>1 ) {
+        } else if (m > 1) {
             hrSize = dec.format(m).concat(" MB");
-        } else if ( k>1 ) {
+        } else if (k > 1) {
             hrSize = dec.format(k).concat(" KB");
         } else {
             hrSize = dec.format(b).concat(" Bytes");
--- app/src/main/java/com/example/galleryversionone/SendFilesFragment.java
+++ app/src/main/java/com/example/galleryversionone/SendFilesFragment.java
@@ -2,9 +2,9 @@ package com.example.galleryversionone;
 
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -13,26 +13,31 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
-import java.text.DecimalFormat;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 import static com.example.galleryversionone.FileUtils.formatFileSize;
+import static com.example.galleryversionone.FileUtils.getThumbnailInputStream;
 
 public class SendFilesFragment extends Fragment {
 
@@ -42,6 +47,8 @@ public class SendFilesFragment extends Fragment {
     public HashMap<Long, ImageDocument> selectedImages = new HashMap<>();
 
     private TextView sendFilesMsg;
+    private Button btn;
+    private static final String TAG = "send files fragment";
 
     public static SendFilesFragment newInstance() {
         return new SendFilesFragment();
@@ -67,6 +74,14 @@ public class SendFilesFragment extends Fragment {
 
         sendFilesMsg = activity.findViewById(R.id.txt_send_files_info);
 
+        btn = activity.findViewById(R.id.btn_read_file_data);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                readDataFromOutputStream();
+            }
+        });
+
 //        mViewModel = new ViewModelProvider(this).get(SendFilesViewModel.class);
         mainViewModel = new ViewModelProvider(getActivity()).get(MainViewModel.class);
 
@@ -87,65 +102,148 @@ public class SendFilesFragment extends Fragment {
     }
 
     public void writeDataToOutputStream() {
-        WriteInBg writeData = new WriteInBg();
-        writeData.start();
-    }
-
-    public class WriteInBg extends Thread {
 
         String socketFile = "socket.ser";
 
-        @Override
-        public void run() {
-            try {
-                /* create an object from file*/
-                OutputStream socketOut = new FileOutputStream(new File(getActivity().getExternalFilesDir(null), socketFile));
-                ObjectOutputStream socketOutput = new ObjectOutputStream(socketOut);
+        try {
+            /* create an object from file*/
+            OutputStream socketOut = new FileOutputStream(new File(getActivity().getExternalFilesDir(null), socketFile));
+            ObjectOutputStream socketOutput = new ObjectOutputStream(socketOut);
 
-                for (Map.Entry<Long, ImageDocument> file : selectedImages.entrySet()) {
+            for (Map.Entry<Long, ImageDocument> file : selectedImages.entrySet()) {
 
-                    ImageDocument obj = file.getValue();
+                ImageDocument obj = file.getValue();
 
-                    System.out.println(obj.fileDisplayName);
-                    File fileMetaData;
-                    InputStream in;
-                    BufferedInputStream bin;
+                InputStream in;
+                BufferedInputStream bin;
 
-                    /* 1 represent the file read preparation flag  */
-                    socketOutput.writeInt(1);
-                    /* 2 represent the object is of type raw file */
-                    socketOutput.writeInt(2);
-                    socketOutput.writeObject(obj);
+                /* 1 represent the file read preparation flag  */
+                socketOutput.writeInt(1);
+                /* 2 represent the object is of type file */
+                socketOutput.writeInt(2);
+                socketOutput.writeObject(obj);
+                /* 3 represent the image thumbnail */
+                socketOutput.writeInt(3);
 
-                    in = getActivity().getContentResolver().openInputStream(Uri.parse(obj.fileContentUri));
-                    bin = new BufferedInputStream(in);
+                in = getThumbnailInputStream(getContext().getContentResolver(), obj.id);
+                bin = new BufferedInputStream(in);
+                /* writing thumbnail file size */
+                socketOutput.writeLong(in.available());
 
-                    /* writing file from buffer reader into socket.bin Object output stream */
-                    byte[] buffer = new byte[1024];
-                    int bytReads;
-                    while ((bytReads = bin.read(buffer)) != -1) {
-                        socketOutput.write(buffer, 0, bytReads);
-                    }
-                    socketOutput.reset();
-
-                    /* closing input streams */
-                    /*socketOutput.flush();*/
-                    bin.close();
-                    in.close();
+                /* writing file from buffer reader into socket.bin Object output stream */
+                byte[] buffer = new byte[1024];
+                int bytReads = 0;
+                while ((bytReads = bin.read(buffer)) != -1) {
+                    socketOutput.write(buffer, 0, bytReads);
                 }
+                socketOutput.reset();
 
-                /* closing output stream with int 0 */
-                socketOutput.writeInt(0);
-                socketOutput.flush();
-                socketOutput.close();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                /* used to handle closed all open connection in case exception throws */
+                /* closing input streams */
+                bin.close();
+                in.close();
+
+                /* 4 represent the image file */
+                socketOutput.writeInt(4);
+
+                in = getActivity().getContentResolver().openInputStream(Uri.parse(obj.fileContentUri));
+                bin = new BufferedInputStream(in);
+
+                /* writing file from buffer reader into socket.bin Object output stream */
+                bytReads = 0;
+                while ((bytReads = bin.read(buffer)) != -1) {
+                    socketOutput.write(buffer, 0, bytReads);
+                }
+                socketOutput.reset();
+
+                /* closing input streams */
+                /*socketOutput.flush();*/
+                bin.close();
+                in.close();
             }
+
+            /* closing output stream with int 0 */
+            socketOutput.writeInt(0);
+            socketOutput.flush();
+            socketOutput.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            /* used to handle closed all open connection in case exception throws */
         }
+    }
+
+    public void readDataFromOutputStream() {
+
+        String socketFile = "socket.ser";
+
+        try {
+
+            InputStream sinput = new FileInputStream(new File(getActivity().getExternalFilesDir(null), socketFile));
+            ObjectInputStream sobj = new ObjectInputStream(sinput);
+
+            while (sobj.readInt() == 1) {
+                ImageDocument fileObj;
+                OutputStream out;
+                BufferedOutputStream bout;
+
+                if (sobj.readInt() == 2) {
+                    fileObj = (ImageDocument) sobj.readObject();
+
+                    byte[] buffer = new byte[1024];
+                    int bytReads = 0;
+                    int length = 0;
+
+                    if (sobj.readInt() == 3) {
+                        out = new FileOutputStream(new File(getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES), "thumbnail_" + fileObj.fileDisplayName));
+                        bout = new BufferedOutputStream(out);
+                        length = (int) sobj.readLong();
+                        while (length > 0) {
+                            bytReads = sobj.read(buffer);
+                            length -= bytReads;
+                            bout.write(buffer, 0, bytReads);
+                            Log.i(TAG, "readDataFromOutputStream: " + bytReads + ":" + sobj.available());
+                        }
+                        Log.i(TAG, "readDataFromOutputStream: " + bytReads + " " + sobj.available());
+                        bout.flush();
+                        bout.close();
+                        out.close();
+                    }
+
+                    if (sobj.readInt() == 4) {
+                        out = new FileOutputStream(new File(getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES), fileObj.fileDisplayName));
+                        bout = new BufferedOutputStream(out);
+
+                        bytReads = 0;
+                        length = fileObj.fileSize.intValue();
+                        while (length > 0) {
+                            bytReads = sobj.read(buffer);
+                            length -= bytReads;
+                            bout.write(buffer, 0, bytReads);
+                            Log.i(TAG, "readDataFromOutputStream: " + bytReads + ":" + sobj.available());
+                        }
+                        Log.i(TAG, "readDataFromOutputStream: " + bytReads + " " + sobj.available());
+                        bout.flush();
+                        bout.close();
+                        out.close();
+                    }
+
+                }
+            }
+
+            sobj.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (EOFException e) {
+            Log.i(TAG, "readDataFromOutputStream: end of file reached");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 
 
